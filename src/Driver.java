8613046e49ce8b6bbac6494c8d2cda3d13720112
Jin Ruan <jinruan1800@gmail.com>
2016-10-11 19:24:49 -0500

    Fix path bugs in Driver

--- src/Driver.java
+++ src/Driver.java
@@ -9,30 +9,29 @@ public class Driver {
             args3: output directory for NormalizeCoOccurrenceMatrix job
             args4: output directory for MultiplicationMapperJoin job
             args5: output directory for MultiplicationSum job
-            args6: file representing user rating matrix
-            args7: file of movie title
-            args8: output directory for RecommenderListGenerator job
+            args6: file of movie title
+            args7: output directory for RecommenderListGenerator job
         */
-		
+
 		DataDividerByUser dataDividerByUser = new DataDividerByUser();
 		CoOccurrenceMatrixGenerator coOccurrenceMatrixGenerator = new CoOccurrenceMatrixGenerator();
 		NormalizeCoOccurrenceMatrix normalizeCoOccurrenceMatrix = new NormalizeCoOccurrenceMatrix();
 		MultiplicationMapperJoin mapperJoin = new MultiplicationMapperJoin();
 		MultiplicationSum multiplicationSum = new MultiplicationSum();
 		RecommenderListGenerator generator = new RecommenderListGenerator();
-		
+
 		String[] path1 = {args[0], args[1]};
 		String[] path2 = {args[1], args[2]};
 		String[] path3 = {args[2], args[3]};
 		String[] path4 = {args[3], args[0], args[4]};
-        String[] path5 = {args[4], args[5]};
-		String[] path6 = {args[6], args[7], args[5], args[8]};
-		
+		String[] path5 = {args[4], args[5]};
+		String[] path6 = {args[6], args[5], args[7]};
+
 		dataDividerByUser.main(path1);
 		coOccurrenceMatrixGenerator.main(path2);
 		normalizeCoOccurrenceMatrix.main(path3);
 		mapperJoin.main(path4);
-        multiplicationSum.main(path5);
+		multiplicationSum.main(path5);
 		generator.main(path6);
 	}
 
