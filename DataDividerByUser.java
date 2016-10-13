e290d185e910b596d3e5f77563fa9e128e6126f5
Jin Ruan <jinruan1800@gmail.com>
2016-10-13 00:11:19 -0500

    Initial commit

---
+++ DataDividerByUser.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class DataDividerByUser {
+	public static class DataDividerMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+			//input user,movie,rating
+			String[] user_movie_rating = value.toString().trim().split(",");
+			int userID = Integer.parseInt(user_movie_rating[0]);
+			String movieID = user_movie_rating[1];
+			String rating = user_movie_rating[2];
+
+			context.write(new IntWritable(userID), new Text(movieID + ":" + rating));
+		}
+	}
+
+	public static class DataDividerReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
+		// reduce method
+		@Override
+		public void reduce(IntWritable key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+
+			StringBuilder sb = new StringBuilder();
+			while (values.iterator().hasNext()) {
+				sb.append("," + values.iterator().next());
+			}
+			//key = user value=movie1:rating, movie2:rating...
+			context.write(key, new Text(sb.toString().replaceFirst(",", "")));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		Configuration conf = new Configuration();
+
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(DataDividerMapper.class);
+		job.setReducerClass(DataDividerReducer.class);
+
+		job.setJarByClass(DataDividerByUser.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
+
+		TextInputFormat.setInputPaths(job, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+# Movie Recommender System
+
+## Overview
+In this project, I built a movie recommender system based on Item Collaborative Filtering using Hadoop MapReduce in Java.
+
+## Data
+Data comes from Netflix Prize Challenge, which has the following format:
+
+User_id, Movie_id, Rating  
+1,10001,5.0  
+2,10001,2.0  
+3,10001,2.0  
+4,10001,5.0  
+5,10001,4.0
+
+## Step
+
+* Divide data by user id
+* Build co-occurrence matrix
+* Build rating matrix
+* Multiply co-occurrence matrix and rating matrix
+* Generate recommendation list
+
+## How to run
+
+I deployed a hadoop cluster on Docker, which has one namenode and two datanodes, and tested my program on it.
+
+```
+$hadoop com.sun.tools.javac.Main *.java
+$jar cf recommender.jar *.class
+$hadoop jar recommender.jar Driver /input /dataDividedByUser /coOccurrenceMatrix /Multiplication /coOccurrenceMatrix/part-r-00000 /input/userRating.txt /MovieTitle/movieTitles.txt /Recommender
+```
+
+* args0: original dataset
+* args1: output directory for DividerByUser job
+* args2: output directory for coOccurrenceMatrixBuilder job
+* args3: output directory for Multiplication job
+* args4: file representing co-occurrence matrix
+* args5: file representing user rating matrix
+* args6: file of movie title 
+* args7: output directory for RecommenderListGenerator job
+
+
